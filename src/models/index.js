dafa864bde296d683773278e2e4d9c6518226fca
AsadGuluzade <guluzade.asad@gmail.com>
2023-01-26 20:41:35 +0000

    added classes

--- src/models/index.js
+++ src/models/index.js
@@ -1,8 +1,18 @@
-const User = require('./user')
+class Car {
+  constructor(make, model, year, price, photo) {
+    this.make = make
+    this.model = model
+    this.year = year
+    this.price = price
+    this.photo = photo
+  }
+}
+const car = new Car('Toyota', 'Camry', 2019, 30000, 'photo.url')
+const car1 = new Car('Honda', 'Civic', 2020, 35000, 'photo1.url')
+const car2 = new Car('Ford', 'Mustang', 2021, 45000, 'photo2.url')
 
-const asad = new User('armagan', 20)
-const steve = new User('steve', 22)
+const cars = [car, car1, car2]
 
-const users = [asad, steve]
-
-module.exports = { users }
+/* class User{
+  constructor()
+} */
